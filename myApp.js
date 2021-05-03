fcd8207e61dd9e2240adab5978e9ba3af6cd3810
Amulya <iitdelhi2017@gmail.com>
2021-05-03 08:30:53 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,5 +1,18 @@
 var express = require('express');
 var app = express();
+console.log("Hello World");
+
+var greet=function(req,res){
+  res.send("Hello Express");
+};
+
+var sendfile=function(req,res){
+  abs_path=__dirname+"/views/index.html";
+  res.sendFile(abs_path);
+};
+//app.get("/",greet);
+
+app.get("/",sendfile);
 
 
 
